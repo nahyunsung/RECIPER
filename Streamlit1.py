d4c5c5539479eafad3eb1ed5dfffe3d3d00fdcd1
nahyunsung <54356259+nahyunsung@users.noreply.github.com>
2022-10-15 00:57:48 +0900

    Add files via upload

--- Streamlit1.py
+++ Streamlit1.py
@@ -1,10 +1,16 @@
 import streamlit as st
 import pandas as pd
-view = [150, 250, 30]
-st.write("# Youtube view")
-st.write("## raw")
-view
-st.write("## bar chart")
-st.bar_chart(view)
-sview = pd.Series(view)
-sview
+
+st.write("# RECIPER")
+Genders = ["남성", "여성"]
+GenderRadio = st.radio("자신의 성을 고르세요", Genders)
+age = st.slider("몇 살이세요?", 0, 100, 50)
+Food_datas = pd.read_csv('Food_data.csv', encoding='utf-8')
+
+if GenderRadio == "남성":
+    st.write("남성 환영 ", age)
+else:
+    st.write("여성 환영 ", age)
+
+sview = pd.Series(Food_datas)
+sview.head()
