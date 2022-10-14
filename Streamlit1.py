0d9eeaec72ef0387e24ae057c1eb425153ed94dc
nahyunsung <54356259+nahyunsung@users.noreply.github.com>
2022-10-15 01:32:59 +0900

    Add files via upload

--- Streamlit1.py
+++ Streamlit1.py
@@ -1,16 +1,18 @@
 import streamlit as st
 import pandas as pd
+import csv
 
 st.write("# RECIPER")
 Genders = ["남성", "여성"]
 GenderRadio = st.radio("자신의 성을 고르세요", Genders)
 age = st.slider("몇 살이세요?", 0, 100, 50)
-Food_datas = pd.read_csv('Food_data.csv', encoding='utf-8')
+Food_File = open("Food_data.csv", "r")
+Food_Data = csv.reader(Food_File)
 
 if GenderRadio == "남성":
     st.write("남성 환영 ", age)
 else:
     st.write("여성 환영 ", age)
 
-sview = pd.Series(Food_datas)
-sview.head()
+for x in range(1, 6):
+    st.write(Food_Data[x])
