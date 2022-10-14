323244e0b793581f29c794c4ce1eec4feb143c82
nahyunsung <54356259+nahyunsung@users.noreply.github.com>
2022-10-15 01:45:43 +0900

    Add files via upload

--- Streamlit1.py
+++ Streamlit1.py
@@ -5,11 +5,9 @@ st.write("# RECIPER")
 Genders = ["남성", "여성"]
 GenderRadio = st.radio("자신의 성을 고르세요", Genders)
 age = st.slider("몇 살이세요?", 0, 100, 50)
-Food_File = pd.read_csv("Food_data.csv", header=0)
 
 if GenderRadio == "남성":
     st.write("남성 환영 ", age)
 else:
     st.write("여성 환영 ", age)
 
-Food_File
