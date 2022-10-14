69d9ed4dc54c357ef1bcb4b28b095c53a76d27e9
nahyunsung <54356259+nahyunsung@users.noreply.github.com>
2022-10-15 03:55:30 +0900

    Add files via upload

--- Streamlit1.py
+++ Streamlit1.py
@@ -31,15 +31,18 @@ Genders = ["남성", "여성"]
 GenderRadio = st.radio("자신의 성을 고르세요", Genders)
 age = st.slider("몇 살이세요?", 0, 100, 50)
 
-if GenderRadio == "남성":
-    st.write("남성 환영 ", age)
-else:
-    st.write("여성 환영 ", age)
-
 FoodFile = pd.read_csv("Food_data.csv", encoding="utf-8")
-
-st.checkbox("Use container width", value=False, key="use_container_width")
-
 df = load_data()
 
+
+if GenderRadio == "남성":
+    st.write("남성 환영 ", age)
+    df.sort_values("남성", ascending=False)
+else:
+    st.write("여성 환영 ", age)
+    df.sort_values("여성", ascending=False)
+
+st.checkbox("너비 맞추기", value=False, key="use_container_width")
+
 st.dataframe(df, use_container_width=st.session_state.use_container_width)
+
