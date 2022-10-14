d57048848926118d2e59e0a756d7aeca44ccad12
nahyunsung <54356259+nahyunsung@users.noreply.github.com>
2022-10-15 01:52:15 +0900

    Add files via upload

--- Streamlit1.py
+++ Streamlit1.py
@@ -6,8 +6,9 @@ Genders = ["남성", "여성"]
 GenderRadio = st.radio("자신의 성을 고르세요", Genders)
 age = st.slider("몇 살이세요?", 0, 100, 50)
 
+FoodData = pd.read_excel("Food_Data.xlsx", sheet_name=1, engine="openpyxl")
+
 if GenderRadio == "남성":
     st.write("남성 환영 ", age)
 else:
     st.write("여성 환영 ", age)
-
