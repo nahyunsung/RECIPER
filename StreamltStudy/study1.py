78fe28b25e47d2230e11089f1b7ca218c2dbb510
nahyunsung <54356259+nahyunsung@users.noreply.github.com>
2023-11-09 01:09:47 +0900

    카메라

--- StreamltStudy/study1.py
+++ StreamltStudy/study1.py
@@ -1,29 +1,4 @@
 import streamlit as st
-import cv2
-import numpy as np
-import tempfile
+from streamlit_webrtc import webrtc_streamer
 
-cap = cv2.VideoCapture(1)
-
-st.title("Video Capture with OpenCV")
-
-frame_placeholder = st.empty()
-
-stop_button_pressed = st.button("stop")
-
-while cap.isOpened() and not stop_button_pressed:
-    ret, frame = cap.read()
-
-    if not ret:
-        st.write("The.video capture has ended.")
-        break
-
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-    frame_placeholder.image(frame, channels="RGB")
-
-    if cv2.waitKey(1) & 0xFF == ord("q") or stop_button_pressed:
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+webrtc_streamer(key="key")
