cc5ce4eb6c2eecccbe09615b5c486d6ad43a80db
nahyunsung <54356259+nahyunsung@users.noreply.github.com>
2023-11-09 01:41:07 +0900

    카메라

--- StreamltStudy/study1.py
+++ StreamltStudy/study1.py
@@ -1,7 +1,13 @@
+import folium
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer
 
-st.title("My first Streamlit app")
-st.write("Hello, world")
+from streamlit_folium import st_folium
 
-webrtc_streamer(key="example")
+# center on Liberty Bell, add marker
+m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+folium.Marker(
+    [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
+).add_to(m)
+
+# call to render Folium map in Streamlit
+st_data = st_folium(m, width=725)
