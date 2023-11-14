7d8838c1fd7949e90a6872ccaeed55753941d11d
nahyunsung <54356259+nahyunsung@users.noreply.github.com>
2023-11-15 01:18:29 +0900

    카메라

--- StreamltStudy/study1.py
+++ StreamltStudy/study1.py
@@ -4,10 +4,12 @@ import streamlit as st
 from streamlit_folium import st_folium
 
 # center on Liberty Bell, add marker
-m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
-folium.Marker(
-    [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
-).add_to(m)
+latData = 37.689140353876326 # 안양공업고등학교 위도
+lonData = 127.754518810868 # 안양공업고등학교 경도
+
+center = [37.541, 126.986]
+
+m = folium.Map(location=center, zoom_start=12)
 
 # call to render Folium map in Streamlit
 st_data = st_folium(m, width=725)
