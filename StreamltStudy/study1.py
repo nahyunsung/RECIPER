a47f039a27d52c6d993730861c93e6cd70e73017
nahyunsung <54356259+nahyunsung@users.noreply.github.com>
2023-11-17 10:58:24 +0900

    Update study1.py

--- StreamltStudy/study1.py
+++ StreamltStudy/study1.py
@@ -1,13 +1,11 @@
-import folium
-import streamlit as st
-
 from streamlit_folium import st_folium
+import folium
 
-center = [37.5010,127.0509] 
-# center on Seoul pharmacy
-m = folium.Map(location=center, zoom_start=18)
-markers = plugins.MarkerCluster(transformed_coord_list)
-markers.add_to(m) 
+df = pd.read_csv("cat_ho_list.csv", encoding='cp949')
+latData = 37.689140353876326 # 안양공업고등학교 위도
+lonData = 127.754518810868 # 안양공업고등학교 경도
 
-# call to render Folium map in Streamlit
+center = [37.541, 126.986]
+
+m = folium.Map(location=center, zoom_start=12)
 st_data = st_folium(m, width=725)
