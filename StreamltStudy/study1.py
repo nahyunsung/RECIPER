072a92d11628a8146f0d3e88bd296948ff7e15a9
nahyunsung <54356259+nahyunsung@users.noreply.github.com>
2023-11-17 10:59:25 +0900

    Update study1.py

--- StreamltStudy/study1.py
+++ StreamltStudy/study1.py
@@ -1,9 +1,5 @@
 from streamlit_folium import st_folium
 import folium
-
-latData = 37.689140353876326 # 안양공업고등학교 위도
-lonData = 127.754518810868 # 안양공업고등학교 경도
-
 center = [37.541, 126.986]
 
 m = folium.Map(location=center, zoom_start=12)
