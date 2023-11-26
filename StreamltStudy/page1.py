6734eada469926ffe084c54af38dc91cf4154121
nahyunsung <54356259+nahyunsung@users.noreply.github.com>
2023-11-26 21:32:36 +0900

    카메라

--- StreamltStudy/page1.py
+++ StreamltStudy/page1.py
@@ -1,5 +1,41 @@
 import streamlit as st
-import time
-import numpy as np
 
-st.set_page_config(page_title="Plotting Demo", page_icon="📈")
+from streamlit_option_menu import option_menu
+
+import study1, page2
+
+st.set_page_config(
+    page_title="CAT",
+)
+
+class MultiApp:
+
+    def __init__(self):
+        self.apps = []
+    def add_app(self, title, function):
+        self.apps.append({
+            "title": title,
+            "function": function
+        })
+
+    def run(self):
+        with st.sidebar:
+            app = option_menu(
+                menu_title='Pondering',
+                options=['Home', 'Camera'],
+                icons=['house-fill', 'camera-video-fill'],
+                menu_icon='chat-text-fill',
+                default_index=1,
+                styles={
+                    "container": {"padding": "5!important", "background-color":'black'},
+                    "icon": {"color": "white", "font-size": "23px"},
+                    "nav-link": {"color": "white", "font-size": "20px", "text-align": "left", "margin":"0px"},
+                    "nav-link-selected": {"background-color": "#02ab21"},}
+            )
+
+            if app=='Home':
+                study1.app()
+            if app == 'Camera':
+                page2.app()
+
+    run()
