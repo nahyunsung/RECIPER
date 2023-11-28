4516a263d81dce7bc2e5e165c760d5a062126911
nahyunsung <54356259+nahyunsung@users.noreply.github.com>
2023-11-28 13:30:36 +0900

    Update study1.py

--- StreamltStudy/study1.py
+++ StreamltStudy/study1.py
@@ -18,8 +18,8 @@ def app():
 
 
     
-    for idx, geo_df_row in df.iterrows() :
-        html = """
+    for idx,let geo_df_row in df.iterrows() :
+        let html = """
             <h1>${geo_df_row['보호센터명']}</h1><br>
             With a few lines of code...
             <p>geo_df_row['전화번호']
@@ -29,7 +29,8 @@ def app():
                 
             </code>
             </p>
-            """
+        """
+        $('#cards-box').append(temp_html)
         
         iframe = branca.element.IFrame(html=html, width=500, height=300)
         popup = folium.Popup(iframe, max_width=500)
