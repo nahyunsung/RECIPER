212fdfea931be132589fd15cee8376c3bea5d700
nahyunsung <54356259+nahyunsung@users.noreply.github.com>
2023-11-28 10:35:07 +0900

    Update study1.py

--- StreamltStudy/study1.py
+++ StreamltStudy/study1.py
@@ -1,8 +1,17 @@
 import streamlit as st
 from PIL import Image
+import folium
+from streamlit_folium import st_folium
 
 def app():
     st.title('길고양이 증가에 따른 생태계 파괴')
     st.text('길 고양이를 볼때마다 사진을 찍어 인근 동물보호소에 연략을 줄 수 있는 사이트')
     image = Image.open('StreamltStudy/cat1.png')
     st.image(image)
+    
+    m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+    folium.Marker(
+        [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
+    ).add_to(m)
+    
+    st_data = st_folium(m, width=725)
