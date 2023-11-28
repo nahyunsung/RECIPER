3a8d23d967ab12234b11942d14e069e0b392e5ac
nahyunsung <54356259+nahyunsung@users.noreply.github.com>
2023-11-28 14:34:30 +0900

    Update study1.py

--- StreamltStudy/study1.py
+++ StreamltStudy/study1.py
@@ -5,7 +5,6 @@ from streamlit_folium import st_folium
 from folium import plugins
 import pandas as pd
 import branca
-from flask import Flask, render_template_string
 
 def app():
     st.title('길고양이 증가에 따른 생태계 파괴')
