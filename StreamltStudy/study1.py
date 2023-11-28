a315213cffb421931b4c0660abbebb47202e1543
nahyunsung <54356259+nahyunsung@users.noreply.github.com>
2023-11-28 13:28:56 +0900

    Update study1.py

--- StreamltStudy/study1.py
+++ StreamltStudy/study1.py
@@ -20,7 +20,7 @@ def app():
     
     for idx, geo_df_row in df.iterrows() :
         html = """
-            <h1>#{geo_df_row['보호센터명']}</h1><br>
+            <h1>${geo_df_row['보호센터명']}</h1><br>
             With a few lines of code...
             <p>geo_df_row['전화번호']
             <code>
