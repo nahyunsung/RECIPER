41f3b68edc87d6a20f8a814bac175e1bda6417a8
nahyunsung <54356259+nahyunsung@users.noreply.github.com>
2023-11-28 21:32:19 +0900

    Update page2.py

--- StreamltStudy/page2.py
+++ StreamltStudy/page2.py
@@ -48,7 +48,7 @@ def app():
 def detect_objects(image, model):
     image_resized = tf.image.resize(np.array(image), (224, 224))
     image_normalized = (tf.cast(image_resized, tf.float32) / 127.0) - 1.0
-    image_reshaped = image_normalized.reshape((1, 224, 224, 3))
+    image_reshaped = tf.reshape(image_normalized, (1, 224, 224, 3))
 
     # Make predictions
     prediction = model.predict(image_reshaped)
