33f639dcb87ff9916c978804193ff4bc3388fab3
nahyunsung <54356259+nahyunsung@users.noreply.github.com>
2023-11-28 12:45:39 +0900

    Update study1.py

--- StreamltStudy/study1.py
+++ StreamltStudy/study1.py
@@ -4,7 +4,6 @@ import folium
 from streamlit_folium import st_folium
 from folium import plugins
 import pandas as pd
-import numpy
 
 def app():
     st.title('길고양이 증가에 따른 생태계 파괴')
@@ -15,24 +14,17 @@ def app():
     df = pd.read_csv("cat_url_list.csv", encoding='UTF8')
     df = df.dropna()
     m = folium.Map(location=[37.564214, 127.001699], zoom_start=12)
-    icon_image = numpy.array(Image.open("aniicon.png"))
-    shadow_image = numpy.array(Image.open("aniicon.png"))
-    
-    icon = folium.CustomIcon(
-        icon_image,
-        icon_size=(38, 95),
-        icon_anchor=(22, 94),
-        shadow_image=shadow_image,
-        shadow_size=(50, 64),
-        shadow_anchor=(4, 62),
-        popup_anchor=(-3, -76),
+
+    custom_icon = folium.CustomIcon(
+        icon_image='aniicon.png',  # 사용자 지정 이미지 파일의 경로
+        icon_size=(30, 30)  # 이미지의 크기
     )
     
     for idx, geo_df_row in df.iterrows() :
         marker = folium.Marker(
             location=[geo_df_row["위도"], geo_df_row["경도"]],
             popup=geo_df_row['전화번호'],  # 마커에 표시될 설명
-            icon=icon  # 마커의 아이콘 설정
+            icon=custom_icon  # 마커의 아이콘 설정
         )
         
         # 마커를 Folium 맵에 추가
