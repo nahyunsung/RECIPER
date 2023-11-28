f31a0abec34635449a5d6be0fd4f4b47dfde7671
nahyunsung <54356259+nahyunsung@users.noreply.github.com>
2023-11-28 10:14:09 +0900

    Update study1.py

--- StreamltStudy/study1.py
+++ StreamltStudy/study1.py
@@ -1,6 +1,8 @@
 import streamlit as st
+from PIL import Image
 
 def app():
     st.title('길고양이 증가에 따른 생태계 파괴')
     st.text('길 고양이를 볼때마다 사진을 찍어 인근 동물보호소에 연략을 줄 수 있는 사이asd트')
-    st.image('cat1.png')
+    image = Image.open('cat1.png')
+	st.image(image)
