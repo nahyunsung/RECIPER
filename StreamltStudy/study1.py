5207ae3a036b6041ffc4515650cb74f7c7de4e71
nahyunsung <54356259+nahyunsung@users.noreply.github.com>
2023-11-28 12:37:13 +0900

    Update study1.py

--- StreamltStudy/study1.py
+++ StreamltStudy/study1.py
@@ -14,12 +14,24 @@ def app():
     df = pd.read_csv("cat_url_list.csv", encoding='UTF8')
     df = df.dropna()
     m = folium.Map(location=[37.564214, 127.001699], zoom_start=12)
+    icon_image = Image.open("aniicon.png")
+    shadow_image = Image.open("leaf-shadow.png")
+    
+    icon = folium.CustomIcon(
+        icon_image,
+        icon_size=(38, 95),
+        icon_anchor=(22, 94),
+        shadow_image=shadow_image,
+        shadow_size=(50, 64),
+        shadow_anchor=(4, 62),
+        popup_anchor=(-3, -76),
+    )
     
     for idx, geo_df_row in df.iterrows() :
         marker = folium.Marker(
             location=[geo_df_row["위도"], geo_df_row["경도"]],
             popup=geo_df_row['전화번호'],  # 마커에 표시될 설명
-            icon=folium.Icon(color='blue')  # 마커의 아이콘 설정
+            icon=icon  # 마커의 아이콘 설정
         )
         
         # 마커를 Folium 맵에 추가
