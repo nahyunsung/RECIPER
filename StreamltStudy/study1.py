5c1ce855e6b30a2161085880826c4fb2f56551c0
nahyunsung <54356259+nahyunsung@users.noreply.github.com>
2023-11-29 09:21:55 +0900

    Update study1.py

--- StreamltStudy/study1.py
+++ StreamltStudy/study1.py
@@ -7,7 +7,8 @@ import pandas as pd
 import branca
 
 def app():
-    st.title('길고양이 증가에 따른 생태계 파괴')
+    image = Image.open('homeimage3.jpg')
+    st.image(image)
     st.text('길 고양이를 볼때마다 사진을 찍어 인근 동물보호소에 연략을 줄 수 있는 사이트')
     image = Image.open('StreamltStudy/cat1.png')
     st.image(image)
