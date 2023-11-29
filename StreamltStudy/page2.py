a1cca2e16526afe80330fe1e8f33b2971bed66d6
nahyunsung <54356259+nahyunsung@users.noreply.github.com>
2023-11-29 21:00:02 +0900

    Update page2.py

--- StreamltStudy/page2.py
+++ StreamltStudy/page2.py
@@ -41,3 +41,8 @@ def app():
         
         
         st.write(readLines[prediction_result][0])
+        if readLines[prediction_result][0] == "0":
+            image = Image.open('sodog.jpg')
+        else:
+            image = Image.open('find.jpg')
+        st.image(image)
